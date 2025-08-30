b5824cf60a3783c739c58bb4630fffcd7514c100
PABLO HENRIQUIE CARDOSO RAMOS <pablohenriquevickinn1@gmail.com>
2025-08-30 19:50:33 -0300

    CONSTRUÇÃO INICIAL DO MENU. V1.1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,36 @@
+import locale
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import os
+from datetime import datetime
+from starlette.responses import FileResponse
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/static/{path:path}")
+async def server_static(path: str):
+    file_path = os.path.join("app/static", path)
+
+    if os.path.exists(file_path):
+        if path.endswith(".css"):
+            return FileResponse(file_path, media_type="text/css")
+        return FileResponse(file_path)
+    return {"erro": "Arquivo não encontrado"}
 
 @app.get("/menu")
 def exibir_menu(request: Request):
-    return templates.TemplateResponse("menu.html", {"request": request})
+    nome_usuario = "Pablo" # APRIMORAR ARA RECONHECER NO LOGIN
+    dias_semana = [
+        "SEGUNDA-FEIRA", "TERÇA-FEIRA", "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SÁBADO", "DOMINGO"
+    ]
+    dia_atual = dias_semana[datetime.now().weekday()]
+
+    return templates.TemplateResponse(
+        "menu.html",
+        {"request": request,
+         "nome_usuario": nome_usuario,
+         "dia_atual": dia_atual
+         })
