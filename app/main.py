6203b91b343d45d215b26d8528bb4a29546c9715
PABLO HENRIQUIE CARDOSO RAMOS <pablohenriquevickinn1@gmail.com>
2025-09-06 18:59:04 -0300

    CONSTRUÇÃO INICIAL DO MENU. V1.2

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 import locale
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
 from datetime import datetime
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, RedirectResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
+
+# (LUCAS VAI SUBSTITUIR E IMPLEMENTAR COM O BANCO DE DADOS)
+treinos = [
+    {"id": 1, "nome": "Treino A", "exercicios": [
+        {"nome": "Supino Reto", "repeticoes": 10, "series": 4},
+    ]},
+
+    {"id": 2, "nome": "Treino B", "exercicios": [
+        {"nome": "Agachamento Livre", "repeticoes": 10, "series": 4},
+    ]}
+]
 
 @app.get("/static/{path:path}")
 async def server_static(path: str):
@@ -34,3 +45,25 @@ def exibir_menu(request: Request):
          "nome_usuario": nome_usuario,
          "dia_atual": dia_atual
          })
+
+@app.get("/treinos")
+def listar_treinos(request: Request):
+
+    return templates.TemplateResponse(
+        "telaTreino.html",
+        {"request": request, "treinos": treinos}
+    )
+
+@app.post("/treinos/adicionar")
+def adicionar_treino(nome: str = Form( ... )):
+    novo_id = max(t["id"] for t in treinos) + 1 if treinos else 1
+    treinos.append({"id": novo_id, "nome": nome, "exercicios": []})
+    return RedirectResponse("/treinos", status_code=303)
+
+@app.post("/treinos/{treino_id}/exercicios/adicionar")
+def adicionar_exercicio(treino_id: int, nome: str = Form(...), repeticoes: int = Form(...), series: int = Form(...)):
+    for t in treinos:
+        if t["id"] == treino_id:
+            t["exercicios"].append({"nome": nome, "repeticoes": repeticoes, "series": series})
+
+        return RedirectResponse("/treinos", status_code=303)
